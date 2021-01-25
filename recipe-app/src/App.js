8611078948f78140ccde8a6f8b0ef929eb71c443
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-25 22:29:47 +0200

    playing around with useState functionality

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = 'f0cf7372';
+  const APP_KEY = '605e21ecc6b0822b7868ce895ae88362';
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect has been run');
+  });
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+        <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
