7cfde09b7e2a13f6ec395c5ed4582b2ffce746c3
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-26 01:18:00 +0200

    finished js, adding css

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -8,18 +8,20 @@ const App = () => {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('banana');
+
 
   useEffect(() => {
     getRecipes();
-  }, [search]);
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/api/food-database/v2/parser?nutrition-type=logging&ingr=canned&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/api/food-database/v2/parser?nutrition-type=logging&ingr=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
+    console.log(data)
     setRecipes(data.hints);
-    console.log(data.hints);
     // another way to write everything
     // fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
     // .then (repsonse => {
@@ -28,24 +30,31 @@ const App = () => {
 
   const updateSearch = e => {
     setSearch(e.target.value)
-    console.log(search);
-  }
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search)
+    setSearch('');
+  };
 
   return (
     <div className="App">
-      <form className="search-form">
+      <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button type="submit" className="search-button">
           Search
         </button>
       </form>
-      {recipes.map(recipe => (
-        <Recipe
-          key={recipe.food.label}
-          title={recipe.food.label}
-          image={recipe.food.image}
-          calories={recipe.food.nutrients.ENERC_KCAL} />
-      ))}
+      <div className='recipes'>
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.food.label}
+            title={recipe.food.label}
+            image={recipe.food.image}
+            calories={recipe.food.nutrients.ENERC_KCAL} />
+        ))}
+      </div>
     </div>
   );
 };
